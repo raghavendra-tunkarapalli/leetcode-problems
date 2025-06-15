a94856eaf7feeed425366352a81cc4aad736968d
Raghavendra Tunkarapalli <theraghuvaran@gmail.com>
2025-06-15 08:50:29 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int[] arr = new int[5];
+    for(int i=0;i<arr.length;i++){
+      arr[i] = sc.nextInt[];
+    Solution obj1 = new Solution();
+    List<List<Integer>> res = obj1.threeSum();
+    System.out.print(res);
+  }
+}
