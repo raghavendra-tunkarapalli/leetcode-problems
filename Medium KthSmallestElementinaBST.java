2e2a27ad78fc4450e576ee115f13553fe5de7b8b
Raghavendra Tunkarapalli <theraghuvaran@gmail.com>
2025-08-21 07:31:38 +0530

    Create Medium KthSmallestElementinaBST.java

---
+++ Medium KthSmallestElementinaBST.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int result = -1;
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root,k);
+        return result;
+    }
+    public void dfs(TreeNode root,int k){
+        if(root==null)return;
+        dfs(root.left,k);
+        count++;
+        if(count==k){
+            result=root.val;
+            return;
+        }
+        dfs(root.right,k);
+    }
+}
